fe42b771e1500294e199643b6625c362bfa51e10
kevbp <legauloisfr@gmail.com>
2016-01-04 10:46:03 +0100

    connexion

--- monstership-dev/monstership/monstership-ejb/src/main/java/com/monstership/service/MemberManager.java
+++ monstership-dev/monstership/monstership-ejb/src/main/java/com/monstership/service/MemberManager.java
@@ -6,6 +6,8 @@ import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import java.util.logging.Logger;
 
 //The @Stateless annotation eliminates the need for manual transaction demarcation
@@ -22,12 +24,18 @@ public class MemberManager {
     private Event<Member> memberEventSrc;
 
     public void connect(Member member) throws Exception {
-        log.info("Connecting " + member.getPseudo());
-        // TODO
+    	Query q = em.createQuery("from Member m where m.pseudo=:ps and m.password=:pw");
+    	q.setParameter("ps", member.getPseudo());
+    	q.setParameter("pw", member.getPassword());
+    	if(q.getResultList().isEmpty()) {
+        	log.info("ERROR Connecting " + member);
+        	throw new Exception();
+        }
+        log.info("Connecting " + member);
     }
 
     public void register(Member member) throws Exception {
-        log.info("Registering " + member.getPseudo());
+        log.info("Registering " + member);
         em.persist(member);
         memberEventSrc.fire(member);
     }
