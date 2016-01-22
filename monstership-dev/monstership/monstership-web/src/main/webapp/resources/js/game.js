4eb2e165f92da2e5c6aca7c3fa48a3f72e772aca
kevbp <legauloisfr@gmail.com>
2016-01-22 18:49:30 +0100

    Pleins de trucs : une police d écriture pour l'affichage des planètes, du javascript pour afficher les action points, un peu de css pour l affichage de la map.

--- monstership-dev/monstership/monstership-web/src/main/webapp/resources/js/game.js
+++ monstership-dev/monstership/monstership-web/src/main/webapp/resources/js/game.js
@@ -1,11 +1,52 @@
-var size = 20;
-var table = "<table class='map'>";
-for(var y=0; y < size; y++) {
-    table += "<tr>";
-    for(var x=0; x < size; x++) {
-        table += "<td x='"+x+"' y='"+y+"'> </td>";
+function showMap() {
+    var size = 19;
+    var table = "<table class='map'>";
+    for(var y=0; y < size; y++) {
+        table += "<tr>";
+        for(var x=0; x < size; x++) {
+            if(x == Math.floor(size/2) && y == x) {
+                table += "<td onclick='move("+x+","+y+")'>O</td>";
+            }
+            else {
+                table += "<td onclick='move("+x+","+y+")'></td>";
+            }
+        }
+        table += "</tr>";
     }
-    table += "</tr>";
+    table += "</table>";
+    document.getElementById('showMap').innerHTML = table;
 }
-table += "</table>";
-document.getElementById('showMap').innerHTML = table;
+
+function showStarshipInformations() {
+    $.getJSON( "../rest/game/starship", function( data ) {
+        document.getElementById('showStarshipPosition').innerHTML = "Position ["+data.xPos+","+data.yPos+"]<br/>Action points: "+data.actionPoint;
+    });
+}
+
+function move(x, y) {
+    var dir = null;
+    if (y < 9 && x == 9) {
+        dir = "UP";
+    }
+    else if (y > 9 && x == 9) {
+        dir = "DOWN";
+    }
+    else if (x < 9 && y == 9) {
+        dir = "LEFT";
+    }
+    else if (x > 9 && y == 9) {
+        dir = "RIGHT";
+    }
+    else {
+        // not allowed
+    }
+
+    if (dir != null) {
+        $.get( "../rest/game/move/" + dir, function( data ) {
+        });
+        showStarshipInformations();
+        showMap();
+    }
+}
+showStarshipInformations();
+showMap();
